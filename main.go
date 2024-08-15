10e6b2415c7d5c9037685bb345994c91ea85b5fb
ankur-jod <ankur.mukherjee.22@aot.edu.in>
2024-08-16 00:47:46 +0530

    yes

--- main.go
+++ main.go
@@ -16,8 +16,17 @@ func main() {
 	var version = 0.01 // only veriable declearation
 	fmt.Println(version)
 
+	/* Types of Variable -
+
+	 */
+
+	const pi = 6.45
+	fmt.Print(pi)
 	var a = 9
 	var b = 6
 	fmt.Println(a + b)
+
+	ankur := 143
+	fmt.Println(ankur)
 
 }
---
+++ print/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	age := 20
+	name := "Ankur"
+	height := 120
+
+	fmt.Println("", age, "", name, "", height)
+	fmt.Println("Hi")
+
+	fmt.Printf("%d", age)
+	// If you use fmt.printf() then use format specifiers like %d ,%f,%t etc
+	fmt.Printf("\n")
+	fmt.Printf("Ankur")
+}
