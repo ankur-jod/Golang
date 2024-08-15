d654cf4ce009132e533d00b803ee804ac16c86bd
ankur-jod <ankur.mukherjee.22@aot.edu.in>
2024-08-15 18:25:50 +0530

    yes

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	//"mylearning/myutil"
+)
+
+func main() {
+	fmt.Println("Hi")
+	fmt.Println("Hi")
+	//myutil.PrintMessage("Welcome to my world")
+
+	var name string = "ankur"
+	fmt.Println(name)
+
+	var version = 0.01
+	fmt.Println(version)
+}
